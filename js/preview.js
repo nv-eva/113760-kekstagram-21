e46a055d627d282afce8f6759fff2dbe6d2baeb3
nv-eva <ds222591@rambler.ru>
2020-11-06 17:27:27 +0300

    Добавляет загрузку по 5 комментариев

--- js/preview.js
+++ js/preview.js
@@ -2,7 +2,10 @@
 
 (function () {
   const bigPicture = document.querySelector(`.big-picture`);
-  window.bigPicture = bigPicture;
+  const commentsList = bigPicture.querySelector(`.social__comments`);
+  const loaderComments = bigPicture.querySelector(`.comments-loader`);
+
+  const COUNT_COMMENTS = 5;
 
   const renderComment = function (comment) {
     const photoComment = document.createElement(`li`);
@@ -24,20 +27,48 @@
     return photoComment;
   };
 
+  /*
+  const hideCounterComments = function () {
+    const counterComments = window.bigPicture.querySelector(`.social__comment-count`);
+    counterComments.classList.add(`hidden`);
+  };
+  */
+
+  const updateComments = function (comments) {
+    let fragmentLength;
+
+    if (comments.length <= COUNT_COMMENTS) {
+      fragmentLength = comments.length;
+      loaderComments.classList.add(`hidden`);
+    } else {
+      fragmentLength = COUNT_COMMENTS;
+      loaderComments.classList.remove(`hidden`);
+    }
+
+    const photoComments = document.createDocumentFragment();
+    for (let i = 0; i < fragmentLength; i++) {
+      const comment = renderComment(comments[0]);
+      photoComments.appendChild(comment);
+      comments.shift();
+    }
+    commentsList.appendChild(photoComments);
+  };
+
   window.renderBigPicture = function (photo) {
     bigPicture.querySelector(`.big-picture__img img`).src = photo.url;
     bigPicture.querySelector(`.likes-count`).textContent = photo.likes;
     bigPicture.querySelector(`.comments-count`).textContent = photo.comments.length;
     bigPicture.querySelector(`.social__caption`).textContent = photo.description;
 
-    const commentsList = bigPicture.querySelector(`.social__comments`);
     commentsList.textContent = ``;
 
-    const photoComments = document.createDocumentFragment();
-    for (let j = 0; j < photo.comments.length; j++) {
-      const comment = renderComment(photo.comments[j]);
-      photoComments.appendChild(comment);
-    }
-    commentsList.appendChild(photoComments);
+    const loadComments = photo.comments.slice();
+
+    updateComments(loadComments);
+    loaderComments.addEventListener(`click`, function () {
+      updateComments(loadComments);
+    });
   };
+
+  window.bigPicture = bigPicture;
 })();
