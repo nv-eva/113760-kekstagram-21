dace0feb88e53880ca1ec61bae21803613c66ade
nv-eva <ds222591@rambler.ru>
2020-11-06 21:02:01 +0300

    Добавляет загрузку файла

---
+++ js/file.js
@@ -0,0 +1,27 @@
+'use strict';
+
+(function () {
+  const FILE_TYPES = [`jpg`, `jpeg`, `png`];
+
+  const fileChooser = document.querySelector(`#upload-file`);
+  const preview = document.querySelector(`.img-upload__preview img`);
+
+  fileChooser.addEventListener(`change`, function () {
+    const file = fileChooser.files[0];
+    const fileName = file.name.toLowerCase();
+
+    const matches = FILE_TYPES.some(function (it) {
+      return fileName.endsWith(it);
+    });
+
+    if (matches) {
+      const reader = new FileReader();
+
+      reader.addEventListener(`load`, function () {
+        preview.src = reader.result;
+      });
+
+      reader.readAsDataURL(file);
+    }
+  });
+})();
