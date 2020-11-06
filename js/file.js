65ee759482a0dfcc23010a63e8a07a955ee07089
nv-eva <ds222591@rambler.ru>
2020-11-06 21:26:40 +0300

    Заменяет изображение в превью эффектов

--- js/file.js
+++ js/file.js
@@ -5,6 +5,7 @@
 
   const fileChooser = document.querySelector(`#upload-file`);
   const preview = document.querySelector(`.img-upload__preview img`);
+  const effectsPreview = document.querySelectorAll(`.effects__preview`);
 
   fileChooser.addEventListener(`change`, function () {
     const file = fileChooser.files[0];
@@ -19,6 +20,10 @@
 
       reader.addEventListener(`load`, function () {
         preview.src = reader.result;
+
+        for (let i = 0; i < effectsPreview.length; i++) {
+          effectsPreview[i].style.backgroundImage = `url(${reader.result})`;
+        }
       });
 
       reader.readAsDataURL(file);
